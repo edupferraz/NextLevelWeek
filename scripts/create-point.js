c83811653c7ad592c67580489700a26e6ef5fa15
edupferraz <edupferraz@gmail.com>
2020-06-03 11:40:25 -0300

    Third_Day - Next_Level_Week

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,5 @@
+
+//Função responsável por listar estados
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
@@ -14,6 +16,8 @@ function populateUFs() {
 
 populateUFs()
 
+
+//Função responsável por chamar as cidades de acordo com o estado escolhido
 function getCities(event) {
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
@@ -32,11 +36,10 @@ function getCities(event) {
 
     fetch(url)
     .then( res => res.json() )
-    .then( cities => {
-        
+    .then( cities => {  
 
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -45,4 +48,54 @@ function getCities(event) {
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+    .addEventListener("change", getCities);
+
+
+
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect){
+
+    item.addEventListener("click", handleSelectedItem)
+
+}
+
+    const collectedItems = document.querySelector("[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+
+    const item = event.target
+
+    // adicionar ou remover uma classe em javascript
+    item.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id
+
+
+    //verificar se existem itens selecionados
+
+
+    //se sim - pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex( item => item == itemId)
+
+    
+    //se já estiver - tirar da seleção
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item =>item != itemId)
+
+        selectedItems = filteredItems
+    } else{
+        // se não - adicionar a seleção
+
+        selectedItems.push(itemId)
+    }
+
+    //atualizar o input escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+    
+}
